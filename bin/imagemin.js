6ba94581b20b9e8625eea9eadc095161dadf239b
Allan Ebdrup <allan@878.dk>
2020-05-02 21:37:41 +0200

    half the size of images, and compress webp

--- bin/imagemin.js
+++ bin/imagemin.js
@@ -1,18 +1,53 @@
+const fs = require('fs');
+const glob = require('glob');
+const sharp = require('sharp');
 const imagemin = require('imagemin');
 const imageminPngquant = require('imagemin-pngquant');
 const webp = require('imagemin-webp'); // imagemin's WebP plugin.
 
-const destination = './img/min'; // Output folder
 (async () => {
-  await imagemin(['img/*.png'], {
+  await Promise.all(
+    glob.sync('img/*.png', { absolute: true }).map(async file => {
+      const outputFile = file.replace('/img/', '/img/half/');
+      if (file === outputFile) {
+        throw new Error(`Problem with file: ${file}, gives outputFile: ${outputFile}`);
+      }
+      const input = fs.readFileSync(file);
+      const { width } = await sharp(input).metadata();
+      let newWidth = Math.round(width * 0.5);
+      if (newWidth < 500) {
+        newWidth = width;
+      }
+      console.log({ width, newWidth, outputFile });
+      await sharp(input).resize().toFile(outputFile);
+    }),
+  );
+  const destination = './img/min'; // Output folder
+
+  const files = await imagemin(['img/half/*.png'], {
+    destination,
+    plugins: [webp({ quality: 50 })],
+  });
+  console.log(
+    files.map(({ data, sourcePath, destinationPath }) => ({
+      sourcePath,
+      destinationPath,
+      byteLength: data.byteLength,
+    })),
+  );
+  const files2 = await imagemin(['img/half/*.png'], {
     destination,
     plugins: [
       imageminPngquant({
         quality: [0.5, 0.7],
       }),
-      webp({
-        lossless: true, // Losslessly encode images
-      }),
     ],
   });
+  console.log(
+    files2.map(({ data, sourcePath, destinationPath }) => ({
+      sourcePath,
+      destinationPath,
+      byteLength: data.byteLength,
+    })),
+  );
 })();
