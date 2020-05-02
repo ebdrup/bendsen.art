817c6b94b5fee330ea3a6c78c8bce2ff1234feb6
Allan Ebdrup <allan@878.dk>
2020-05-02 20:35:42 +0200

    webp

--- bin/imagemin.js
+++ bin/imagemin.js
@@ -1,12 +1,17 @@
 const imagemin = require('imagemin');
 const imageminPngquant = require('imagemin-pngquant');
+const webp = require('imagemin-webp'); // imagemin's WebP plugin.
 
+const destination = './img/min'; // Output folder
 (async () => {
   await imagemin(['img/*.png'], {
-    destination: 'img/min',
+    destination,
     plugins: [
       imageminPngquant({
         quality: [0.5, 0.7],
+      }),
+      webp({
+        lossless: true, // Losslessly encode images
       }),
     ],
   });
--- bin/webp.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-find $1 -name '*.jpg' -exec cwebp -q 80 {} -o {}".webp" \;
-find $1 -name '*.png' -exec cwebp -q 80 {} -o {}".webp" \;
